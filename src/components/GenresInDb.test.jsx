61deb5a72baa65526465fe228167f260a0aab30c
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-12-02 03:55:59 -0600

    Fixed all dependencies problems

--- src/components/GenresInDb.test.jsx
+++ src/components/GenresInDb.test.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import {render, fireEvent, act} from '@testing-library/react'
-
+import {render, fireEvent, act, waitFor, cleanup} from '@testing-library/react'
+import '@testing-library/jest-dom'
 import {server} from "../mocks/browser"
 import {genresData} from "../mocks/handlers";
 import GenresInDb from "./GenresInDb"
+
+// Fix problem https://github.com/testing-library/react-testing-library/issues/731
+import MutationObserver from "mutation-observer";
+global.MutationObserver = MutationObserver;
 
 let component;
 beforeEach(() => {
     component = render(<GenresInDb/>)
 })
 
+afterEach(()=> component.unmount())
+
 
 describe("Testing initial state of the component", () => {
 
     it('GenresInDb is rendered', () => {
-
         expect(component.container).toBeDefined()
     })
 
@@ -25,7 +30,6 @@ describe("Testing initial state of the component", () => {
     })
 
     it('GenresInDb is initially rendered without any genres', () => {
-        // Act
         let genresRow = component.container.querySelector('.row')
         expect(genresRow).toBeEmptyDOMElement()
     })
@@ -33,42 +37,39 @@ describe("Testing initial state of the component", () => {
 })
 
 
-describe("Testing component updates", () => {
+describe("Testing component update", () => {
     beforeAll(() => {
         server.listen()
     })
-
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-
     afterEach(() => server.resetHandlers())
     afterAll(() => server.close())
 
     it("Should update state, show all genres actives in request and hide no active genres", async () => {
-        await act(() => sleep(500))
+        await waitFor(() =>
+            {
+                genresData.data.forEach(genre => {
+                    let genresTitle = component.queryByText(genre.name)
+                    if (genre.active)
+                        expect(genresTitle).toBeInTheDocument()
+                    else
+                        expect(genresTitle).not.toBeInTheDocument()
+                })
+            }
 
-        genresData.data.forEach(genre => {
-
-            let genresTitle = component.queryByText(genre.name)
-            if (genre.active)
-                expect(genresTitle).toBeInTheDocument()
-            else
-                expect(genresTitle).not.toBeInTheDocument()
-        })
+        )
     })
 
     it("Add class bg-secondary to genres on hover", async () => {
-        await act(() => sleep(500))
-
-        genresData.data.forEach(genre => {
-            if (genre.active) {
-                let genresTitle = component.getByText(genre.name)
-                fireEvent.mouseEnter(genresTitle)
-                expect(genresTitle).toHaveClass("bg-secondary")
-                fireEvent.mouseLeave(genresTitle)
-                expect(genresTitle).not.toHaveClass("bg-secondary")
-            }
+        waitFor(() => {
+            genresData.data.forEach(genre => {
+                if (genre.active) {
+                    let genresTitle = component.getByText(genre.name)
+                    fireEvent.mouseEnter(genresTitle)
+                    expect(genresTitle).toHaveClass("bg-secondary")
+                    fireEvent.mouseLeave(genresTitle)
+                    expect(genresTitle).not.toHaveClass("bg-secondary")
+                }
+            })
         })
     })
 })
